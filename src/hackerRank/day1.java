7ea205959b85c032a51c091c21b36f62e23c59ab
Manthanoice <mvbosamiya20@gmail.com>
2021-07-28 19:29:52 +0530

    Initial Commit

---
+++ src/hackerRank/day1.java
@@ -0,0 +1,13 @@
+package hackerRank;
+
+import java.util.*;
+
+public class day1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Hello, World.");
+        String s = sc.nextLine();
+        System.out.println(s);
+    }
+}
+
---
+++ src/hackerRank/day2.java
@@ -0,0 +1,24 @@
+package hackerRank;
+
+import java.io.*;
+import java.util.*;
+
+public class day2 {
+
+    public static void main(String[] args) {
+        int i=4;
+        double d=4.0;
+        String s="HackerRank ";
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        double d1 = sc.nextDouble();
+        sc.nextLine();
+        String s1 = sc.nextLine();
+        int sum = i+n;
+        double sum2 = d+d1;
+        System.out.println(sum);
+        System.out.println(sum2);
+        System.out.println(s+s1);
+    }
+}
+
